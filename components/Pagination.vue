e898cbd7f975bb979de02655ff471030146dc32b
miracleonyenma <miracleficient@gmail.com>
2022-06-13 15:26:09 +0100

    feat: fetch & display posts

---
+++ components/Pagination.vue
@@ -0,0 +1,39 @@
+
+<!-- ./components/Pagination.vue -->
+
+<script setup>
+defineProps(["pagination"]);
+// helper function to create pagination
+const getPagination = ({ page, pageSize, total, pageCount }) => {
+  // Get previous page number with `1` as the lowest
+  let prev = Math.abs(page - 1) > 0 ? Math.abs(page - 1) : 1;
+  // Get next page with number of total pages as highest
+  let next = page + 1 < pageCount ? page + 1 : pageCount;
+  // console.log({ pages, prev, next });
+  return {
+    pageCount, page, pageSize, total, prev, next,};
+};
+</script>
+<template>
+  <div class="pagination">
+    <ul class="list">
+      <li>
+        <a :href="`?page=` + getPagination(pagination).prev">
+          <button class="cta">Previous</button>
+        </a>
+      </li>
+      <li v-for="n in getPagination(pagination).pageCount" :key="n">
+        <a :href="`?page=${n}`">
+          <button class="cta">
+            {{ n }}
+          </button>
+        </a>
+      </li>
+      <li>
+        <a :href="`?page=` + getPagination(pagination).next">
+          <button class="cta">Next</button>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ components/Post.vue
@@ -0,0 +1,129 @@
+<!-- ./components/Post.vue -->
+
+<script setup>
+// import icons
+import { ChatIcon, PaperAirplaneIcon } from "@heroicons/vue/solid";
+
+// define `post` props
+defineProps(["post"]);
+
+const {
+  public: { strapiURL },
+} = useRuntimeConfig();
+
+// helper function to get username from `post` attributes
+const getUsername = (post) => post?.attributes?.user.data?.attributes?.username;
+
+// helper to get and display date
+const showDateTime = (post) => new Date(post?.attributes?.updatedAt).toDateString() + " | " + new Date(post?.attributes?.updatedAt).toLocaleTimeString();
+</script>
+<template>
+  <li class="post">
+    <header class="post-header">
+      <div class="author-cont">
+        <div class="avatar">
+          <!-- Get first letter in user name -->
+          <p>{{ getUsername(post)?.substr(0, 1) }}</p>
+        </div>
+        <p class="username">{{ getUsername(post) }}</p>
+      </div>
+      <!-- Post options go here -->
+    </header>
+    <ul class="photos">
+      <li v-for="photo in post?.attributes?.photo.data" :key="photo.id" class="photo">
+        <div class="img-cont">
+          <img :src="strapiURL + photo?.attributes?.url" :alt="post?.attributes?.caption" />
+        </div>
+      </li>
+    </ul>
+    <p class="caption">{{ post?.attributes?.caption }}</p>
+    <div class="info-cont text-xs text-gray-600">
+      <div class="time">{{ showDateTime(post) }}</div>
+    </div>
+    <div class="comment-cont">
+      <ChatIcon class="icon solid" />
+      <form class="comment-form">
+        <div class="form-control">
+          <textarea name="comment" id="comment" cols="30" rows="1" class="form-textarea comment-textarea" placeholder="Leave a comment"> </textarea>
+        </div>
+        <button class="cta submit-btn">
+          <PaperAirplaneIcon class="icon solid rotate-90" />
+        </button>
+      </form>
+    </div>
+  </li>
+</template>
+
+<style scoped>
+.dropdown {
+  @apply relative;
+}
+
+.dropdown-btn:hover ~ .dropdown-list,
+.dropdown-btn:focus ~ .dropdown-list,
+.dropdown-list:hover {
+  @apply visible;
+}
+
+.dropdown-list {
+  @apply invisible absolute flex flex-col gap-1.5 top-full right-0 bg-white border border-gray-200 shadow-xl rounded-lg overflow-hidden;
+}
+
+.dropdown-list li button {
+  @apply p-2 px-3 hover:bg-gray-100 active:shadow-inner;
+}
+
+.post {
+  @apply flex flex-col gap-2 p-4 bg-white border border-gray-200 rounded-xl;
+}
+
+.author-cont {
+  @apply flex items-center gap-2 pb-4;
+}
+
+.author-cont .avatar {
+  @apply flex items-center justify-center w-8 h-8 text-xl text-white bg-gray-800 rounded-full;
+}
+
+.author-cont .username {
+  @apply font-semibold;
+}
+
+.photos {
+  @apply flex w-full overflow-x-auto;
+  @apply snap-x snap-mandatory;
+}
+
+.photo {
+  @apply min-w-full snap-start snap-always;
+}
+
+.post .post-header {
+  @apply flex items-center justify-between gap-4;
+}
+
+.post .img-cont img {
+  @apply w-full object-cover rounded-xl;
+}
+
+.caption {
+  @apply font-medium;
+}
+
+.comment-cont {
+  @apply flex items-center gap-4 pt-4 border-t border-t-gray-200;
+}
+
+.comment-form {
+  @apply flex gap-2;
+}
+
+.comment-form .comment-textarea {
+  @apply w-full;
+}
+
+.submit-btn {
+  @apply h-min;
+}
+</style>
+
